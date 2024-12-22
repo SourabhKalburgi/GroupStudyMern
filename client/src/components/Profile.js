00f318cc5041e5eb0a8f0f8d5d2fd6478ebc24b2
SourabhKalburgi <54435233+SourabhKalburgi@users.noreply.github.com>
2024-12-22 15:16:28 +0530

    layout and hero changes

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,39 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { Layout } from './Layout';
+import Modal from './Modal';
 import './Profile.css';
 
 const Profile = () => {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(!user);
 
   const handleLogout = () => {
     logout();
     navigate('/auth');
   };
+
   const handleDashboardNavigation = () => {
     navigate('/user-dashboard');
   };
+
+  const handleModalClose = () => {
+    navigate('/'); // Redirect to the home page or any desired route
+    setIsModalOpen(false);
+  };
+
+  const handleLogin = () => {
+    navigate('/auth'); // Redirect to login/signup page
+  };
+
   return (
     <Layout>
-      <div className="profile-container">
-        <h1 className="page-title">Profile</h1>
-        <div className="profile-details">
-          <h2>User Details</h2>
-          <p><strong>Username:</strong> {user.username}</p>
-          <p><strong>Email:</strong> {user.email}</p>
-        </div>
-        <button className="dashboard-button" onClick={handleDashboardNavigation}>
-          Dashboard
-        </button>
-
-        <div className="logout-section">
-          <button className="logout-button" onClick={handleLogout}>
-            Logout
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+        onLogin={handleLogin}
+      />
+      {!isModalOpen && (
+        <div className="profile-container">
+          <h1 className="page-title">Profile</h1>
+          <div className="profile-details">
+            <h2>User Details</h2>
+            <p><strong>Username:</strong> {user?.username}</p>
+            <p><strong>Email:</strong> {user?.email}</p>
+          </div>
+          <button className="dashboard-button" onClick={handleDashboardNavigation}>
+            Dashboard
           </button>
+          <div className="logout-section">
+            <button className="logout-button" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </Layout>
   );
 };
